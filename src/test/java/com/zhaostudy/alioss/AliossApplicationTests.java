7f07ad61accbc08b5982981a8d0a7b83d85b8051
Ryze-Zhao <643080112@qq.com>
2019-07-24 22:09:56 +0800

    5.1    创建存储空间

--- src/test/java/com/zhaostudy/alioss/AliossApplicationTests.java
+++ src/test/java/com/zhaostudy/alioss/AliossApplicationTests.java
@@ -13,7 +13,17 @@ public class AliossApplicationTests {
     @Autowired
     private OSS oss;
 
-
+    /**
+     * 创建存储空间
+     */
+    @Test
+    public void createBucket() {
+        // 创建存储空间，这个名字相当于文件夹名字，
+        // 我配置文件写的是这个名字，我也写回这个名字
+        String bucketName = "resume-test";
+        // 新建存储空间默认为标准存储类型，私有权限。
+        oss.createBucket(bucketName);
+    }
 
 
 }
