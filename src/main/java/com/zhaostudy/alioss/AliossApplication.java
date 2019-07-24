667e988960164e66c4ac7b94972158dd9f7e1999
Ryze-Zhao <643080112@qq.com>
2019-07-24 19:23:33 +0800

    AliOss：初始化

---
+++ src/main/java/com/zhaostudy/alioss/AliossApplication.java
@@ -0,0 +1,13 @@
+package com.zhaostudy.alioss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliossApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliossApplication.class, args);
+    }
+
+}
